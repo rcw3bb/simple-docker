0a4ab43c15a6e855e5008394f5a8d0ebce05c463
Ron Webb <ron@ronella.xyz>
2024-01-25 11:38:18 +1300

    Remove excess tasks used during testing.

--- src/main/groovy/xyz/ronella/gradle/plugin/simple/docker/task/DockerExec.groovy
+++
@@ -1,9 +0,0 @@
-package xyz.ronella.gradle.plugin.simple.docker.task
-
-abstract class DockerExec extends DockerContainer {
-    DockerExec() {
-        super()
-        description = 'A convenience docker exec command.'
-        internalCommand.convention('exec')
-    }
-}
