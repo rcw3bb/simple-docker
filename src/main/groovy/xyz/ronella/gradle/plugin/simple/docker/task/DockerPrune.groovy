c2cf6db53430a154df254854728c4292cdb847b6
Ron Webb <ron@ronella.xyz>
2024-01-25 11:38:35 +1300

    Structure refinement.

--- src/main/groovy/xyz/ronella/gradle/plugin/simple/docker/task/DockerPrune.groovy
+++ src/main/groovy/xyz/ronella/gradle/plugin/simple/docker/task/DockerPrune.groovy
@@ -1,6 +1,6 @@
 package xyz.ronella.gradle.plugin.simple.docker.task
 
-abstract class DockerPrune extends DockerImage{
+abstract class DockerPrune extends DockerImage {
     DockerPrune() {
         super()
         description = 'A convenience task for pruning unused images'
